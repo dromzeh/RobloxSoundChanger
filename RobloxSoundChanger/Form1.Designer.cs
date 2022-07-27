fda6984534cf69afdbbaf85439480a1019772ab0
dromzeh <65048232+dromzeh@users.noreply.github.com>
2022-07-27 21:01:57 +0100

    init

---
+++ RobloxSoundChanger/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RobloxSoundChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.updateButton = new System.Windows.Forms.Button();
+            this.autoLocateButton = new System.Windows.Forms.Button();
+            this.mp3fileBtn = new System.Windows.Forms.Button();
+            this.robloxLocationBtn = new System.Windows.Forms.Button();
+            this.githubLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(466, 137);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(108, 55);
+            this.updateButton.TabIndex = 0;
+            this.updateButton.Text = "Update Audio";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // autoLocateButton
+            // 
+            this.autoLocateButton.Location = new System.Drawing.Point(354, 137);
+            this.autoLocateButton.Name = "autoLocateButton";
+            this.autoLocateButton.Size = new System.Drawing.Size(108, 55);
+            this.autoLocateButton.TabIndex = 1;
+            this.autoLocateButton.Text = "Automatically Locate Roblox Installation";
+            this.autoLocateButton.UseVisualStyleBackColor = true;
+            this.autoLocateButton.Click += new System.EventHandler(this.autoLocateButton_Click);
+            // 
+            // mp3fileBtn
+            // 
+            this.mp3fileBtn.Location = new System.Drawing.Point(12, 21);
+            this.mp3fileBtn.Name = "mp3fileBtn";
+            this.mp3fileBtn.Size = new System.Drawing.Size(562, 39);
+            this.mp3fileBtn.TabIndex = 2;
+            this.mp3fileBtn.Text = "Select OGG file to replace death sound with";
+            this.mp3fileBtn.UseVisualStyleBackColor = true;
+            this.mp3fileBtn.Click += new System.EventHandler(this.mp3fileBtn_Click);
+            // 
+            // robloxLocationBtn
+            // 
+            this.robloxLocationBtn.Location = new System.Drawing.Point(12, 76);
+            this.robloxLocationBtn.Name = "robloxLocationBtn";
+            this.robloxLocationBtn.Size = new System.Drawing.Size(562, 39);
+            this.robloxLocationBtn.TabIndex = 3;
+            this.robloxLocationBtn.Text = "Select Roblox Version Location";
+            this.robloxLocationBtn.UseVisualStyleBackColor = true;
+            this.robloxLocationBtn.Click += new System.EventHandler(this.robloxLocationBtn_Click);
+            // 
+            // githubLabel
+            // 
+            this.githubLabel.AutoSize = true;
+            this.githubLabel.Location = new System.Drawing.Point(12, 179);
+            this.githubLabel.Name = "githubLabel";
+            this.githubLabel.Size = new System.Drawing.Size(241, 13);
+            this.githubLabel.TabIndex = 4;
+            this.githubLabel.Text = "https://github.com/dromzeh/robloxsoundchanger";
+            this.githubLabel.Click += new System.EventHandler(this.githubLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "OGG File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Roblox Version Location";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 201);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.githubLabel);
+            this.Controls.Add(this.robloxLocationBtn);
+            this.Controls.Add(this.mp3fileBtn);
+            this.Controls.Add(this.autoLocateButton);
+            this.Controls.Add(this.updateButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Roblox Sound Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button autoLocateButton;
+        private System.Windows.Forms.Button mp3fileBtn;
+        private System.Windows.Forms.Button robloxLocationBtn;
+        private System.Windows.Forms.Label githubLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RobloxSoundChanger/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RobloxSoundChanger
+{
+    public partial class Form1 : Form
+    {
+        public string mostrecentversion;
+        public string mp3File;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void autoLocateButton_Click(object sender, EventArgs e)
+        {
+            // attempt to automatically locate the most recent roblox installation.
+            string directory = @"C:\Users\" + Environment.UserName + @"\AppData\Local\Roblox\Versions\";
+            // Console.WriteLine(directory);
+            try
+            {
+                if (Directory.Exists(directory))
+                {
+                    var dir = new DirectoryInfo(directory);
+                    // look for the most recent created file.
+                    string mostrecentversion = directory + dir.GetDirectories().OrderByDescending(d => d.LastWriteTimeUtc).First().ToString(); ;
+                    MessageBox.Show("Found the roblox directory and most recent version: " + mostrecentversion);
+                    robloxLocationBtn.Text = mostrecentversion;
+
+                }
+                else
+                {
+                    MessageBox.Show("Unable to locate your Roblox folder, please enter it manually.", "Roblox Sound Changer");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Unable to locate your Roblox folder, please enter it manually.", "Roblox Sound Changer");
+            }
+
+        }
+
+        private void robloxLocationBtn_Click(object sender, EventArgs e)
+        {
+            // select version folder
+            FolderBrowserDialog robloxDir = new FolderBrowserDialog();
+            robloxDir.Description = @"Location should look like C:\Users\username\AppData\Local\Roblox\Versions\";
+            try
+            {
+                if (robloxDir.ShowDialog() == DialogResult.OK)
+                {
+                    string chosenRobloxDir = robloxDir.SelectedPath;
+                    var robloxDirNew = new DirectoryInfo(chosenRobloxDir);
+                    string mostrecentversion = chosenRobloxDir + robloxDirNew.GetDirectories().OrderByDescending(d => d.LastWriteTimeUtc).First().ToString(); ;
+                    MessageBox.Show("Setting " + mostrecentversion + "as most recent version.");
+                    robloxLocationBtn.Text = mostrecentversion;
+                }
+                else
+                {
+                    MessageBox.Show("An error occured and was unable to check for directories.", "Roblox Sound Changer");
+                }
+            } catch // incase a directory with no folders inside was entered..
+            {
+                MessageBox.Show("An error occured and was unable to check for directories.", "Roblox Sound Changer");
+            }
+
+        }
+
+        private void mp3fileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog mp3fileLocation = new OpenFileDialog();
+            if (mp3fileLocation.ShowDialog() == DialogResult.OK)
+            {
+                string mp3File = mp3fileLocation.FileName;
+
+                if (mp3File.Contains("ogg"))
+                {
+                    MessageBox.Show("Selected the ogg file as " + mp3File + ".");
+                    mp3fileBtn.Text = mp3File;
+                }
+                else
+                {
+                    MessageBox.Show("The file " + mp3File + " is not an ogg.");
+                }
+            }
+        }
+
+        private void githubLabel_Click(object sender, EventArgs e)
+        {
+            // open a URL into the web browser.
+            System.Diagnostics.Process.Start("http://github.com/dromzeh/robloxsoundchanger");
+        }
+
+        public void updateButton_Click(object sender, EventArgs e)
+        {
+            string mostrecentversion = robloxLocationBtn.Text;
+            string mp3File = mp3fileBtn.Text;
+            updateTheFile(mostrecentversion, mp3File);
+        }
+
+
+        public void updateTheFile (string mostrecentversion, string mp3File)
+        {
+            Console.WriteLine(mostrecentversion + mp3File);
+
+            if (!string.IsNullOrEmpty(mostrecentversion) && !string.IsNullOrEmpty(mp3File))
+            {
+                // check if ouch.ogg exists in the most recent version\content\sounds.
+                string ouch = mostrecentversion + @"\content\sounds\ouch.ogg";
+
+                if (File.Exists(ouch))
+                {
+                    // if it does, delete it.
+                    File.Delete(ouch);
+                }
+
+                // get the file name of the ogg file.
+                string mp3FileName = Path.GetFileName(mp3File);
+
+                // move the ogg file mostrecentversion\content\sounds
+                string mp3FilePath = mostrecentversion + @"\content\sounds\" + mp3FileName;
+
+                // copy the ogg file to the mp3FilePath.
+                File.Copy(mp3File, mp3FilePath, true);
+
+                // rename the moved ogg file to ouch.ogg.
+                File.Move(mp3FilePath, ouch);
+
+                MessageBox.Show("The ogg file "+ mp3File +" has been updated and moved.");
+
+            }
+            else
+            {
+                MessageBox.Show("There are no selected file locations.");
+            }
+
+        }
+    
+    }
+}
